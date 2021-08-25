9edc5faff6b776e3d8d8fc9d4ba58d9172a49fc3
pshukla9653 <88384289+pshukla9653@users.noreply.github.com>
2021-08-25 19:51:34 +0530

    overview popup design

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,12 +24,18 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
         $brands = DB::table('brands')->where('company_id', Auth()->user()->company_id)->get();
         $regions = DB::table('regions')->where('company_id', Auth()->user()->company_id)->get();
         $departments = DB::table('departments')->where('company_id', Auth()->user()->company_id)->get();
+        if($request->input('search')){
+            $query = $request->input('search');
+            $vehicles = Vehicle::where('registration_number', 'LIKE', '%'. $query. '%')->orderByRaw("CAST(order_number as UNSIGNED) ASC")->get();
+        }
+        else{
         $vehicles = Vehicle::orderByRaw("CAST(order_number as UNSIGNED) ASC")->get();
+        }
         return view('home', compact('brands','regions','departments','vehicles'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,12 @@
     padding: 7px;
     border: 1px solid #bbb8b8;
     border-radius: 2px;
+    display: block;
 }
 .tooltip-inner{
-  background-color: #fff!important;
+  
+  background-color: #fff !important;
+  padding:0px !important;
   
 }
 </style>
@@ -35,7 +38,7 @@
 
 <div class="col-md-4" style="padding: 30px;">
 <form class="example" action="#">
-  <input type="text" placeholder="Search.." name="search">
+  <input type="text" placeholder="Search.." name="search" style="height:33px;">
   <button type="submit"><i class="fa fa-search"></i></button>
   </form>		
 </div>
@@ -51,15 +54,15 @@
       <div class="col-md-2">
         <div class="form-group">
           <strong>Start Date</strong>
-          {!! Form::date('adoption_date', null, array('class' =>
-          'form-control custom-modal-textbox', 'id' =>'adoption_date','style' =>'padding:0px!important;')) !!}
-          <div class="text-danger" id="adoption_date"></div>
+          {!! Form::date('start_date', null, array('class' =>
+          'form-control custom-modal-textbox', 'id' =>'start_date','style' =>'padding:0px!important;','autofocus'=>'autofocus')) !!}
+          <div class="text-danger" id="start_date"></div>
         </div> 
       </div>
       <div class="col-md-2">
         <div class="form-group">
           <strong>Date Range</strong>
-              <select name="date_range" id="date_range" style="width:90%;" class="form-control custom-modal-textbox">
+              <select name="date_range" id="date_range" style="width:90%;padding:5px;" class="form-control custom-modal-textbox">
                 <option value="1">1 weeks</option>
                 <option value="2">2 weeks</option>
                 <option value="3">3 weeks</option>
@@ -71,8 +74,8 @@
       <div class="col-md-2">
         <div class="form-group">
           <strong>Brand</strong>
-              <select name="brand_id" id="brand_id" style="width:90%;" class="form-control custom-modal-textbox">
-               
+              <select name="brand_id" id="brand_id" style="width:90%;padding:5px;" class="form-control custom-modal-textbox">
+                <option value="">All Brands</option>
                 @foreach ($brands as $key=>$brand)
                 <option value="{{$brand->id}}">{{$brand->brand_name}}</option>
                 @endforeach
@@ -83,8 +86,8 @@
       <div class="col-md-2">
         <div class="form-group">
           <strong>Department</strong>
-            <select name="department_id" style="width:90%;" id="department_id" class="form-control custom-modal-textbox">
-              
+            <select name="department_id" style="width:90%;padding:5px;" id="department_id" class="form-control custom-modal-textbox">
+                <option value="">All Departments</option>
                   @foreach ($departments as $key=>$department)
                   <option value="{{$department->id}}">{{$department->department_name}}</option>
                   @endforeach
@@ -94,8 +97,8 @@
       <div class="col-md-2">
         <div class="form-group">
           <strong>Region</strong>
-            <select name="region_id" style="width:90%;" id="region_id" class="form-control custom-modal-textbox">
-                
+            <select name="region_id" style="width:90%;padding:5px;" id="region_id" class="form-control custom-modal-textbox">
+              <option value="">All Regions</option>
                 @foreach ($regions as $key=>$region)
                 <option value="{{$region->id}}">{{$region->region_name}}</option>
                 @endforeach
@@ -129,24 +132,14 @@
             <tr>
               <th style="width: 7%; font-weight: 600;">VEHICLES</th>
               @for($i=1; $i< 32; $i++)
-                <th style="width:3% !impotant;padding:10px; font-size:8px;font-weight: 600;
-                @if($i==3  || $i==4) 
-                background-color:#fbf8a0
-                @endif
-                @if($i==10  || $i==11) 
-                background-color:#a9a0fb
-                @endif
-                @if($i==25  || $i==26) 
-                background-color:#efbaba
-                @endif
-                ;">{{$i}}.07.<br>2021</th>
+                <th style="width:3% !impotant;padding:10px; font-size:8px;font-weight: 600;">{{$i}}.07.<br>2021</th>
               @endfor
             </tr>
           </thead>
           <tbody>
             @foreach ($vehicles as $key => $vehicle)
             <tr>
-              <td style="width: 7%; font-weight: 600;"><span data-popup="tooltip" title="
+              <td style="width:7%;"><span data-popup="tooltip" title="
                 <div style='background-color:#fff;width:800px;height:auto;padding:10px;border: 1px solid #bbb8b8;'>
                   <table cellpadding = '10' cellspacing = '10'>
                     <tr><td colspan='3' style='text-align:center;color:#376473;font-weight:600;'>Vehicle Information</td></tr>
@@ -157,59 +150,48 @@
                           <tr><td colspan='2'>&nbsp;</td></tr>
                           <tr><td colspan='2' style='text-align:center;'><span style='font-size:18px;color:black;padding:5px;border-radius: 5px;background-color:{{$vehicle->registration_plate_colour}}'>{{$vehicle->registration_number}}</span></td></tr>
                           <tr><td colspan='2'>&nbsp;</td></tr>
-                          <tr><td><b>Brand:</b></td><td> {{ $vehicle->brand->brand_name }}</td></tr>
-                          <tr><td><b>Model:</b></td><td> {{ $vehicle->model }}</td></tr>
-                          <tr><td><b>Derivative:</b></td><td> {{ $vehicle->derivative }}</td></tr>
-                          <tr><td><b>Start Date:</b></td><td>10-Aug-2021</td></tr>
-                          <tr><td><b>End Date:</b></td><td>11-Aug-2021</td></tr>
-                          <tr><td><b>Primary Contact:</b></td><td>Donato Bilancia</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Brand:</b></td><td> {{ $vehicle->brand->brand_name }}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Model:</b></td><td> {{ $vehicle->model }}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Derivative:</b></td><td> {{ $vehicle->derivative }}</td></tr>
+                          
                           
                         </table>
                       </td>
-                      <td style='width:33.33%;padding:10px; '>
-                        <table style='width:100%'>
-                          <tr style='background-color:{{$vehicle->registration_plate_colour}}'><td><b>Start Date:</b></td><td>10-Aug-2021</td></tr>
-                          <tr style='background-color:{{$vehicle->registration_plate_colour}}'><td><b>End Date:</b></td><td>11-Aug-2021</td></tr>
-                          <tr style='background-color:{{$vehicle->registration_plate_colour}}'><td><b>Primary Contact:</b></td><td>Donato Bilancia</td></tr>
-                          <tr><td><b>Department:</b></td><td>{{ $vehicle->department->department_name }}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Registration NO:</b></td><td>{{$vehicle->registration_number}}</td></tr>
-                          <tr><td><b>VIN:</b></td><td>{{$vehicle->vin}}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Adoption Date:</b></td><td>{{$vehicle->adoption_date}}</td></tr>
-                          <tr><td><b>Projected Defleet Date:</b></td><td>{{$vehicle->projected_defleet_date}}</td></tr>
+                      <td style='width:33.33%;padding:10px;'>
+                        <table style='width:100%;'>
+                          <tr style='font-size:12px; background-color:{{$vehicle->registration_plate_colour}}'><td style='padding:2px;'><b>Brand:</b></td><td> {{ $vehicle->brand->brand_name }}</td></tr>
+                          <tr style='font-size:12px; background-color:{{$vehicle->registration_plate_colour}}'><td style='padding:2px;'><b>Model:</b></td><td> {{ $vehicle->model }}</td></tr>
+                          <tr style='font-size:12px; background-color:{{$vehicle->registration_plate_colour}}'><td style='padding:2px;'><b>Derivative:</b></td><td> {{ $vehicle->derivative }}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Department:</b></td><td>{{ $vehicle->department->department_name }}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Registration NO:</b></td><td>{{$vehicle->registration_number}}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>VIN:</b></td><td>{{$vehicle->vin}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Adoption Date:</b></td><td>{{$vehicle->adoption_date}}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Projected Defleet Date:</b></td><td>{{$vehicle->projected_defleet_date}}</td></tr>
                         </table>
                       </td>
                       <td style='width:33.33%;padding:10px;'>
                         <table style='width:100%'>
                           
-                          <tr style='background-color:#eeeeee;'><td><b>Regions:</b></td><td>{{ $vehicle->region->region_name }}</td></tr>
-                          <tr><td><b>Other Details:</b></td><td>{{$vehicle->other_details}}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Lead Time:</b></td><td>{{$vehicle->lead_time}}</td></tr>
-                          <tr><td><b>Lag Time:</b></td><td>{{$vehicle->lag_time}}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Engine:</b></td><td>{{$vehicle->engine}}</td></tr>
-                          <tr><td><b>Colour:</b></td><td>{{$vehicle->colour}}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Mileage:</b></td><td>{{$vehicle->mileage}}</td></tr>
-                          <tr><td><b>Value:</b></td><td>{{$vehicle->value}}</td></tr>
-                          <tr style='background-color:#eeeeee;'><td><b>Notes:</b></td><td>{{$vehicle->notes}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Regions:</b></td><td>{{ $vehicle->region->region_name }}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Other Details:</b></td><td>{{$vehicle->other_details}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Lead Time:</b></td><td>{{$vehicle->lead_time}}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Lag Time:</b></td><td>{{$vehicle->lag_time}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Engine:</b></td><td>{{$vehicle->engine}}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Colour:</b></td><td>{{$vehicle->colour}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Mileage:</b></td><td>{{$vehicle->mileage}}</td></tr>
+                          <tr style='font-size:12px;'><td style='padding:2px;'><b>Value:</b></td><td>{{$vehicle->value}}</td></tr>
+                          <tr style='font-size:12px;background-color:#eeeeee;'><td style='padding:2px;'><b>Notes:</b></td><td>{{$vehicle->notes}}</td></tr>
                         </table>
                       </td>
                     </tr>
 
                   </table>
                 </div>
-                " data-html="true" data-placement="right" style="padding: 5px;border-radius: 5px;background-color:{{$vehicle->registration_plate_colour}}">{{$vehicle->registration_number}}</span>
-              <br>{{$vehicle->vin}}</td>
+                " data-html="true" data-placement="right" style="font-weight: 600; font-size:12px; padding: 5px;border-radius: 5px;background-color:{{$vehicle->registration_plate_colour}}">{{$vehicle->registration_number}}</span>
+              <br><span style="font-size:9px;position: absolute;margin-top: 7px;">{{$vehicle->vin}}</span>
+              <br><span style="font-size:9px;position: absolute;margin-top: -4px;">Newspress {{$vehicle->model}}</span></td>
               @for($i=1; $i< 32; $i++)
-                <td style="
-                @if($i==3  || $i==4) 
-                background-color:#fbf8a0
-                @endif
-                @if($i==10  || $i==11) 
-                background-color:#a9a0fb
-                @endif
-                @if($i==25  || $i==26) 
-                background-color:#efbaba
-                @endif
-                "></td>
+                <td></td>
               @endfor
             </tr>
             @endforeach
