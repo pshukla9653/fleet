f7ecf2892aba00589922b99e4f8fff6fcb6f8fa5
stefanalbash <stefan.prence@albash.al>
2021-09-28 17:23:16 +0200

    FIX: search

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -17,21 +17,23 @@ class HistoryController extends Controller
         //
         if($request->input('search')){
             $query = $request->input('search');
-            $histories = History::where('user_email', 'LIKE', '%'. $query. '%')->orderBy('id','DESC')->paginate(10);
-           
+            $histories = History::where('user_email', 'LIKE', '%'. $query. '%')
+                ->orWhere('event', 'LIKE', '%'. $query. '%')
+                ->orderBy('id','DESC')->paginate(10);
+
             return view('histories.index', compact('histories'));
-            
+
         }
         else{
         $histories = History::orderBy('id','DESC')->paginate(5);
-        
+
         //var_dump($brands); exit;
         return view('histories.index', compact('histories'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
         }
     }
 
-    
+
     /**
      * Remove the specified resource from storage.
      *
